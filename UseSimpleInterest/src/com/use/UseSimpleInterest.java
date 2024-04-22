975ed46b2c718412b4adb8990df7fc3caa8573d9
SURYA MAURYA <131141621+suryamaurycs@users.noreply.github.com>
2024-04-22 18:48:08 +0530

    Add files via upload

---
+++ UseSimpleInterest/src/com/use/UseSimpleInterest.java
@@ -0,0 +1,12 @@
+package com.use;
+import com.simple.*;
+
+public class UseSimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleInterest si = new SimpleInterest();
+		System.out.println(si.Amount());
+	}
+
+}
---
+++ UseSimpleInterest/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module UseSimpleInterest {
+	requires SimpleInterest;
+}
