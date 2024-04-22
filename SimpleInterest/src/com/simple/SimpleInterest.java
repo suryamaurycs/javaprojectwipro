4b34d88f83cb70331bdac76f31f3aaa6ffd7ee11
SURYA MAURYA <131141621+suryamaurycs@users.noreply.github.com>
2024-04-22 18:46:02 +0530

    Add files via upload

---
+++ SimpleInterest/src/com/simple/SimpleInterest.java
@@ -0,0 +1,7 @@
+package com.simple;
+
+public class SimpleInterest {
+	public float Amount() {
+		return (float) (5000*2.5*5/100);
+	}
+}
---
+++ SimpleInterest/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SimpleInterest {
+	exports com.simple;
+}
